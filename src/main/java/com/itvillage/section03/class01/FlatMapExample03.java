70f71771b1a1240be8eba1dce104cda11214a049
curiousKidd <skasrjhaan@gmail.com>
2024-06-27 22:44:50 +0900

    flatMap update

--- src/main/java/com/itvillage/section03/class01/FlatMapExample03.java
+++ src/main/java/com/itvillage/section03/class01/FlatMapExample03.java
@@ -10,6 +10,11 @@ import reactor.core.scheduler.Schedulers;
  *  - 비동기적으로 동작할 경우, emit 되는 순서를 보장하지 않는다.
  */
 public class FlatMapExample03 {
+    /**
+      * 해당 사항을 이해하기 어려웠는데, 플랫맵과 맵을 같은 로직상에서 비교하니 이해하기가 쉬워짐
+     * 중첩 for문을 한개의 for문 처럼 (평탄화) 해주는 느낌에서의 flat dlek
+      */
+
     public static void main(String[] args) {
         Flux
             .range(2, 8)
@@ -19,6 +24,17 @@ public class FlatMapExample03 {
                                 .map(n -> dan + " * " + n + " = " + dan * n))
 
             .subscribe(Logger::onNext);
+
+        TimeUtils.sleep(200L);
+
+        Flux
+                .range(2, 8)
+                .map(dan -> Flux
+                        .range(1, 9)
+                        .publishOn(Schedulers.parallel())
+                        .map(n -> dan + " * " + n + " = " + dan * n))
+
+                .subscribe(Logger::onNext);
 
         TimeUtils.sleep(200L);
     }
